5f3f86505dade271a9e293767697e4a19da7b276
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-03 11:12:41 -0400

    chore: Optimize font loading in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,24 @@
 import Navbar from '@/components/navbar.component'
+import { Inconsolata, Inter, Roboto } from 'next/font/google'
 import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+})
+
+// const inconsolata = Inconsolata({
+//   subsets: ['latin'],
+// })
+
+// const roboto = Roboto({
+//   subsets: ['latin'],
+//   weight: ['400'],
+// })
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
-      <body>
+      <body className={inter.className}>
         <Navbar />
         <main className='max-w-3xl mx-auto py-10'>{children}</main>
       </body>
