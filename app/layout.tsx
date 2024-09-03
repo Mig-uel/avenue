4bb0b947595007edaa8f0ca51f6c7e88e1bedc2f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-03 11:07:27 -0400

    chore: Update RootLayout component with Navbar and main content container

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
+import Navbar from '@/components/navbar.component'
 import './globals.css'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main className='max-w-3xl mx-auto py-10'>{children}</main>
+      </body>
     </html>
   )
 }
