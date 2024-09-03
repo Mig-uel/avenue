62172306b4c9f3ebfeebb79d55092c3a6a0d6cdc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-03 17:11:42 -0400

    refactor: Update createUser action to use fs/promises for file operations

--- utils/actions.utils.tsx
+++ utils/actions.utils.tsx
@@ -1,7 +1,37 @@
 'use server'
 
-export const createUser = async (formData: FormData) => {
-  const formObject = Object.fromEntries(formData)
+import fs from 'fs/promises'
 
-  console.log(formObject)
+type User = {
+  id?: string
+  firstName?: string
+  lastName?: string
+}
+
+export const createUser = async (formData: FormData) => {
+  const formObject: User = Object.fromEntries(formData)
+
+  const user: User = {
+    firstName: formObject.firstName,
+    lastName: formObject.lastName,
+    id: Date.now().toString(),
+  }
+
+  await saveUser(user)
+}
+
+export const fetchUsers = async (): Promise<User[]> => {
+  const result = await fs.readFile('users.json', { encoding: 'utf8' })
+
+  const users = result ? JSON.parse(result) : []
+
+  return users
+}
+
+const saveUser = async (user: User) => {
+  const users = await fetchUsers()
+
+  users.push(user)
+
+  await fs.writeFile('users.json', JSON.stringify(users))
 }
