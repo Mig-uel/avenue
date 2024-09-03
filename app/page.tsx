5f65cf3dc10908c13be049b2f989a7ad615d73af
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-03 10:53:50 -0400

    chore: Update homepage with link to contact page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,14 @@ const HomePage = () => {
   return (
     <div>
       <h1 className='text-7xl'>HomePage</h1>
-      <Link href='/about' className='text-xl text-blue-500 mt-4'>
+      <Link href='/about' className='text-xl text-blue-500 inline-block mt-4'>
         About
+      </Link>
+      <Link
+        href='/info/contact'
+        className='text-xl text-blue-500 inline-block mt-8'
+      >
+        Contact
       </Link>
     </div>
   )
