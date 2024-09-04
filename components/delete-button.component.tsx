9a494a50c7719e9f687fe5e2577dd8c6bbd9590d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-04 15:16:31 -0400

    refactor: Add DeleteButton component for deleting items

---
+++ components/delete-button.component.tsx
@@ -0,0 +1,14 @@
+const DeleteButton = ({ id }: { id: string }) => {
+  return (
+    <form>
+      <button
+        type='submit'
+        className='bg-red-500 text-white text-xs rounded p-2'
+      >
+        Delete
+      </button>
+    </form>
+  )
+}
+
+export default DeleteButton
