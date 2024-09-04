c4b552155ba4a139037bdaac18d23e5acb7cfb96
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-04 15:16:42 -0400

    feat: Add DeleteButton component for deleting items

--- components/users-list.component.tsx
+++ components/users-list.component.tsx
@@ -1,4 +1,5 @@
 import { fetchUsers } from '@/utils/actions.utils'
+import DeleteButton from './delete-button.component'
 
 const UsersList = async () => {
   const users = await fetchUsers()
@@ -9,8 +10,12 @@ const UsersList = async () => {
       {users.length ? (
         <div>
           {users.map((user) => (
-            <h4 key={user.id} className='capitalize text-lg'>
+            <h4
+              key={user.id}
+              className='capitalize text-lg flex justify-between item-center mb-2'
+            >
               {user.firstName} {user.lastName}
+              <DeleteButton id={user.id as string} />
             </h4>
           ))}
         </div>
