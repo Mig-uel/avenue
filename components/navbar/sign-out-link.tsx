6e3053d9ca08fd54c5905094d633f2f314e9c43b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-05 12:37:48 -0400

    feat: Add sign out functionality to SignOutLink component

--- components/navbar/sign-out-link.tsx
+++ components/navbar/sign-out-link.tsx
@@ -1,5 +1,22 @@
+'use client'
+
+import { useToast } from '@/hooks/use-toast'
+import { SignOutButton } from '@clerk/nextjs'
+
 const SignOutLink = () => {
-  return <div>SignOutLink</div>
+  const { toast } = useToast()
+
+  const handleClick = () => {
+    toast({ description: 'You have been signed out' })
+  }
+
+  return (
+    <SignOutButton redirectUrl='/'>
+      <span className='capitalize w-full text-left' onClick={handleClick}>
+        Logout
+      </span>
+    </SignOutButton>
+  )
 }
 
 export default SignOutLink
