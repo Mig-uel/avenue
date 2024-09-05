16a8c93a5ea8e1fd0e9bd3dd64f1eef7993c04e5
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-04 22:09:48 -0400

    feat: Add LinksDropdown component to Navbar for displaying navigation links
    Add the LinksDropdown component to the Navbar, allowing users to access navigation links in a dropdown menu. This change improves the user experience by providing a more organized and accessible way to navigate the application.

--- components/navbar/links-dropdown.tsx
+++ components/navbar/links-dropdown.tsx
@@ -1,5 +1,35 @@
+import { AlignLeft } from 'lucide-react'
+import { Button } from '../ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+import { UserIcon } from './'
+import { nav_links } from '@/utils/nav-links'
+import Link from 'next/link'
+
 const LinksDropdown = () => {
-  return <div>LinksDropdown</div>
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='outline' className='flex gap-4 max-w-[100px]'>
+          <AlignLeft className='w-6 h-6' />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-52' align='start' sideOffset={10}>
+        {nav_links.map((link, index) => (
+          <DropdownMenuItem key={index}>
+            <Link href={link.href} className='capitalize w-full'>
+              {link.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
 
 export default LinksDropdown
