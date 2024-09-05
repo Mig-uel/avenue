58893b632a00e06178ef0044d9fc895cffa0f75f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-05 11:50:13 -0400

    feat: Update clerkMiddleware to protect non-public routes
    Update the clerkMiddleware function in middleware.ts to include logic for protecting non-public routes. This change enhances the security of the application by ensuring that only authenticated users can access protected routes.

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,11 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(['/', '/rentals'])
 
-export default clerkMiddleware()
+export default clerkMiddleware((auth, req) => {
+  if (isPublicRoute(req)) return
+
+  auth().protect()
+})
 
 export const config = {
   matcher: [
