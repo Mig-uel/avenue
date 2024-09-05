65bbaf5ec1f5b76805b79dea3d7e08644f462c23
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-04 21:43:57 -0400

    feat: Add Providers component and update project structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
 import Navbar from '@/components/navbar/navbar'
+import Providers from './_providers/providers'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -25,13 +26,15 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
+        <Providers>
+          <Navbar />
 
-        <main className='container py-10'>{children}</main>
+          <main className='container py-10'>{children}</main>
+        </Providers>
       </body>
     </html>
   )
