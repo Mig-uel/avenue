01f1962830bf5301f79f391f95bfb3b30a79c83d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-04 21:20:21 -0400

    feat: Add Navbar component and related components
    Add the Navbar component along with DarkMode, LinksDropdown, Logo, NavSearch, SignOutLink, and UserIcon components. These components enhance the navigation and user experience of the application.

---
+++ components/navbar/index.ts
@@ -0,0 +1,6 @@
+export { default as DarkMode } from './dark-mode'
+export { default as LinksDropdown } from './links-dropdown'
+export { default as Logo } from './logo'
+export { default as NavSearch } from './nav-search'
+export { default as SignOutLink } from './sign-out-link'
+export { default as UserIcon } from './user-icon'
