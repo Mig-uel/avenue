8c70bf3207459d10481bf27794a4e7577106dc4c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-06 11:12:19 -0400

    Add user profile schema validation

---
+++ utils/schemas.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+/** USER PROFILE SCHEMA */
+export const profileSchema = z.object({
+  firstName: z.string().min(2),
+  lastName: z.string().min(2),
+  username: z.string().min(4).max(12),
+})
