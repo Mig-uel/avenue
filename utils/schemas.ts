3607d4c74a1a377d7527140be66cfae82ac73b99
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-06 12:36:03 -0400

    Refactor: Update validation messages in profileSchema

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -2,7 +2,10 @@ import { z } from 'zod'
 
 /** USER PROFILE FORMDATA SCHEMA */
 export const profileSchema = z.object({
-  firstName: z.string().min(2),
-  lastName: z.string().min(2),
-  username: z.string().min(4).max(12),
+  firstName: z.string().min(2, { message: 'First name is too short' }),
+  lastName: z.string().min(2, { message: 'Last name is too short' }),
+  username: z
+    .string()
+    .min(4, { message: 'Username is too short' })
+    .max(12, { message: 'Username is too long' }),
 })
