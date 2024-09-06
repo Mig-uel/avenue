f70ce6655a0ab0f1ba32ef1f0d354b07f2de5b65
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-06 11:14:02 -0400

    Refactor: Update comment

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-/** USER PROFILE SCHEMA */
+/** USER PROFILE FORMDATA SCHEMA */
 export const profileSchema = z.object({
   firstName: z.string().min(2),
   lastName: z.string().min(2),
