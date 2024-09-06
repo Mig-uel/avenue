bc13d48ed16981370e847c099a26e31ed7649ff6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-06 12:37:36 -0400

    Refactor: Update createProfileAction to include user profile creation and metadata update

--- utils/actions/profile/actions.ts
+++ utils/actions/profile/actions.ts
@@ -1,27 +1,49 @@
 'use server'
 
+import { clerkClient, currentUser } from '@clerk/nextjs/server'
+import db from '../../db'
 import { profileSchema } from '../../schemas'
 import type { actionFunction } from '@/utils/types'
+import { redirect } from 'next/navigation'
 
 export const createProfileAction: actionFunction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
+    // get current user from clerk
+    const user = await currentUser()
+
+    if (!user) throw new Error('Please login to create a profile.')
+
+    // create object from formdata
     const formDataObject = Object.fromEntries(formData)
 
     // validate formData fields
     const validatedFields = profileSchema.parse(formDataObject)
 
-    console.log(validatedFields)
+    // create new user profile in db using info from clerk and formdata
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    })
 
-    return {
-      message: 'Profile updated.',
-    }
+    // add 'hasProfile' metadata to the users clerk profile
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    })
   } catch (error: unknown) {
-    console.log(error)
     return {
-      message: 'There was an error',
+      message:
+        error instanceof Error ? error.message : 'Something went wrong...',
     }
   }
+
+  return redirect('/')
 }
