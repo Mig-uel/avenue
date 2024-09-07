317a40a8c42e544b4ee3517e83626d2209db529c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-07 08:01:34 -0400

    Refactor: Update SubmitButton component to include a size prop
    - Added a new prop 'size' to the SubmitButton component
    - The 'size' prop can be set to 'default', 'lg', or 'sm'
    - The 'size' prop is used to control the size of the button
    - The default value for 'size' is 'lg'

--- components/form/buttons.component.tsx
+++ components/form/buttons.component.tsx
@@ -8,12 +8,14 @@ type SubmitButtonProps = {
   className?: string
   text?: string
   loadingText?: string
+  size?: 'default' | 'lg' | 'sm'
 }
 
 const SubmitButton = ({
   className = '',
   text = 'submit',
   loadingText = 'Submitting...',
+  size = 'lg',
 }: SubmitButtonProps) => {
   const { pending } = useFormStatus()
 
@@ -22,7 +24,7 @@ const SubmitButton = ({
       type='submit'
       disabled={pending}
       className={`capitalize ${className}`}
-      size='lg'
+      size={size}
     >
       {pending ? (
         <>
