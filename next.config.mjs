968bdaa689cfdf51e04ebd1ae5474220eefb8db4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-07 10:57:58 -0400

    refactor: Update Supabase storage configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ydjyiwtfkiydaqdsjomv.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
