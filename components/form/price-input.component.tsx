d5cb07d288947876e32e9dd8d8a2f08a30cceb63
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-10 17:43:40 -0400

    feat: Add defaultValue prop to PriceInput component

--- components/form/price-input.component.tsx
+++ components/form/price-input.component.tsx
@@ -19,6 +19,7 @@ const PriceInput = ({ defaultValue }: PriceInput) => {
         className='border'
         placeholder='100'
         required
+        defaultValue={defaultValue}
       />
     </div>
   )
