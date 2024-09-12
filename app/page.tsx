8cc31f20918db3618ec8bf0ae7e95a8292a06495
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-12 09:37:37 -0400

    refactor: Update HomePage component to include CategoriesList and PropertiesContainer components
    - Import CategoriesList and PropertiesContainer components
    - Add searchParams prop to HomePage component
    - Render CategoriesList and PropertiesContainer components with searchParams props

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-const HomePage = () => {
-  return <h1 className='text-3xl'>HomePage</h1>
+import CategoriesList from '@/components/home/categories-list.component'
+import PropertiesContainer from '@/components/home/properties-container.component'
+
+const HomePage = ({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string }
+}) => {
+  return (
+    <section>
+      <CategoriesList
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+      <PropertiesContainer
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+    </section>
+  )
 }
 
 export default HomePage
