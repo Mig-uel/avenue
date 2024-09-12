013be94191fe85fc82d258ef78e16d7be1713df6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-12 10:02:25 -0400

    feat: Add PropertiesContainer component to display properties based on category and search filters

---
+++ components/home/properties-container.component.tsx
@@ -0,0 +1,29 @@
+import { fetchProperties } from '@/utils/actions/home/action'
+import { PropertyCard } from '@/utils/types'
+import EmptyList from './empty-list.component'
+import PropertiesList from './properties-list.component'
+
+const PropertiesContainer = async ({
+  category,
+  search,
+}: {
+  category?: string
+  search?: string
+}) => {
+  const properties: PropertyCard[] = await fetchProperties({ category, search })
+
+  return (
+    <>
+      {properties.length ? (
+        <PropertiesList properties={properties} />
+      ) : (
+        <EmptyList
+          heading='No results'
+          message='Try changing or removing some of your filters'
+          buttonText='Clear filters'
+        />
+      )}
+    </>
+  )
+}
+export default PropertiesContainer
