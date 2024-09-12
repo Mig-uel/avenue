6abe0de807edad38b62b0bffe22d2bfa611868ea
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-12 15:43:32 -0400

    refactor: Refactor NavSearch component to include debounced search functionality
    Refactor the NavSearch component to include the useDebouncedCallback hook from the use-debounce package. This allows for debouncing user input in the search field, improving performance and reducing unnecessary API calls. The component also now uses the useRouter and useSearchParams hooks from the next/navigation package to handle updating the URL query parameters based on the search input. Additionally, the placeholder text in the input field has been updated to reflect the purpose of the search functionality.

--- components/navbar/nav-search.tsx
+++ components/navbar/nav-search.tsx
@@ -1,11 +1,44 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useDebouncedCallback } from 'use-debounce'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { Input } from '../ui/input'
 
+// TODO: fetch properties from db, get their names, randomize order, place as placeholder for nav search
+
 const NavSearch = () => {
+  const searchParams = useSearchParams()
+  console.log(searchParams.toString())
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const [search, setSearch] = useState<string>(
+    searchParams.get('search')?.toString() || ''
+  )
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams)
+
+    if (value) {
+      params.set('search', value)
+    } else {
+      params.delete('search')
+    }
+
+    replace(`${pathname}?${params.toString()}`)
+  }, 500)
+
   return (
     <Input
       type='text'
-      placeholder="what's your getaway?..."
+      placeholder='search from thousands of properties...'
       className='max-w-xs dark:bg-muted'
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value)
+        handleSearch(e.target.value)
+      }}
     />
   )
 }
