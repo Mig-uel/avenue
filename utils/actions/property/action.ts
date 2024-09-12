263525b84e51bcda518f92bfbef54cadac283bd0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-12 09:19:35 -0400

    feat: Add createPropertyAction to handle property creation
    - Implement createPropertyAction function to handle property creation.
    - Validate form data and image file using Zod schemas.
    - Upload property image to Supabase bucket.
    - Retrieve authenticated user information.

--- utils/actions/property/action.ts
+++ utils/actions/property/action.ts
@@ -1,10 +1,16 @@
 'use server'
 
+import db from '../../db'
 import type { actionFunction } from '@/utils/types'
 
-import { propertySchema, validateWithZodSchema } from '@/utils/schemas'
+import {
+  imageSchema,
+  propertySchema,
+  validateWithZodSchema,
+} from '@/utils/schemas'
 import { errorMessage, getAuthUser } from '@/utils/functions.utils'
 import { redirect } from 'next/navigation'
+import { uploadImage } from '@/utils/supabase'
 
 export const createPropertyAction: actionFunction = async (
   prevState: any,
@@ -14,17 +20,34 @@ export const createPropertyAction: actionFunction = async (
     const user = await getAuthUser()
 
     const formDataObject = Object.fromEntries(formData)
+    const uploadedFile = formData.get('image') as File
 
     const validatedFields = validateWithZodSchema(
       propertySchema,
       formDataObject
     )
+    const validatedFile = validateWithZodSchema(imageSchema, {
+      image: uploadedFile,
+    })
 
-    // return redirect('/')
-    return {
-      message: 'property created',
-    }
+    // upload property image to supabase bucket
+    const uploadedImagePublicUrl = await uploadImage(
+      validatedFile.image,
+      'properties'
+    )
+
+    // insert new property into supabase db
+    await db.property.create({
+      data: {
+        ...validatedFields,
+        image: uploadedImagePublicUrl,
+        profileId: user.id,
+      },
+    })
+
   } catch (error) {
     return errorMessage(error)
   }
+
+  return redirect('/')
 }
