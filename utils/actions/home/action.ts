8faaa2fa434dbceac14b7fbf5b8f5a014a3fb145
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-12 09:30:41 -0400

    refactor: Update fetchProperties function to include search and category parameters

---
+++ utils/actions/home/action.ts
@@ -0,0 +1,34 @@
+import db from '../../db'
+
+export const fetchProperties = async ({
+  search = '',
+  category,
+}: {
+  search?: string
+  category?: string
+}) => {
+  // find all properties and select certain columns
+  const properties = await db.property.findMany({
+    where: {
+      category,
+      OR: [
+        {
+          name: { contains: search, mode: 'insensitive' },
+          tagline: { contains: search, mode: 'insensitive' },
+        },
+      ],
+    },
+    select: {
+      id: true,
+      name: true,
+      tagline: true,
+      price: true,
+      image: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return properties
+}
