d0d89c827f7bba5235fd7cca44a150288513e4b2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-17 17:52:06 -0400

    refactor: Update buttons component to include CardSignInButton

--- components/form/buttons.component.tsx
+++ components/form/buttons.component.tsx
@@ -3,6 +3,8 @@
 import { useFormStatus } from 'react-dom'
 import { Button } from '../ui/button'
 import { ReloadIcon } from '@radix-ui/react-icons'
+import { SignInButton } from '@clerk/nextjs'
+import { FaRegHeart } from 'react-icons/fa'
 
 type SubmitButtonProps = {
   className?: string
@@ -35,6 +37,22 @@ const SubmitButton = ({
         text
       )}
     </Button>
+  )
+}
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode='modal'>
+      <Button
+        type='button'
+        size='icon'
+        variant='outline'
+        className='p-2 cursor-pointer'
+        asChild
+      >
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
   )
 }
 
