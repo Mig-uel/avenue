4ca7df443c35771cd0a066a3695d34c5aa11590e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-17 17:52:01 -0400

    refactor: Update FavoriteButton component to include authentication check

--- components/card/favorite-button.component.tsx
+++ components/card/favorite-button.component.tsx
@@ -1,7 +1,15 @@
+'use server'
+
+import { auth } from '@clerk/nextjs/server'
 import { FaHeart } from 'react-icons/fa'
 import { Button } from '../ui/button'
+import { CardSignInButton } from '../form/buttons.component'
 
 const FavoriteButton = ({ propertyId }: { propertyId: string }) => {
+  const { userId } = auth()
+
+  if (!userId) return <CardSignInButton />
+
   return (
     <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
       <FaHeart />
