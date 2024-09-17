6e2b3e233ec1be77f12a8b7298425015290836af
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-17 18:22:11 -0400

    refactor: Update FavoriteButtonForm component to include form container and submit button

--- components/card/favorite-button-form.component.tsx
+++ components/card/favorite-button-form.component.tsx
@@ -1,4 +1,31 @@
-const FavoriteButtonForm = () => {
-  return <div>FavoriteButtonForm</div>
+'use client'
+
+import { usePathname } from 'next/navigation'
+import FormContainer from '../form/form-container.component'
+import { CardSubmitButton } from '../form/buttons.component'
+import { toggleFavoriteAction } from '@/utils/actions/favorites/action'
+
+type FavoriteButtonFormProps = {
+  propertyId: string
+  favoriteId: string | null | undefined
+}
+
+const FavoriteButtonForm = ({
+  propertyId,
+  favoriteId,
+}: FavoriteButtonFormProps) => {
+  const pathname = usePathname()
+
+  const toggleAction = toggleFavoriteAction.bind(null, {
+    propertyId,
+    favoriteId,
+    pathname,
+  })
+
+  return (
+    <FormContainer action={toggleAction}>
+      <CardSubmitButton isFavorite={favoriteId ? true : false} />
+    </FormContainer>
+  )
 }
 export default FavoriteButtonForm
