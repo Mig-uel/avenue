31a016f82bbb3b2ef7f3c8d7c867eaf22922fe0c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-17 18:42:46 -0400

    feat: Add dynamic rendering of favorites on FavoritesPage
    - Import EmptyList and PropertiesList components
    - Add fetchFavorites function to fetch favorites data
    - Render PropertiesList component if favorites exist
    - Render EmptyList component if favorites are empty

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,5 +1,19 @@
-const FavoritesPage = () => {
-  return <h1 className='text-3xl'>FavoritesPage</h1>
+import EmptyList from '@/components/home/empty-list.component'
+import PropertiesList from '@/components/home/properties-list.component'
+import { fetchFavorites } from '@/utils/actions/favorites/action'
+
+const FavoritesPage = async () => {
+  const favorites = await fetchFavorites()
+
+  return (
+    <>
+      {favorites.length ? (
+        <PropertiesList properties={favorites} />
+      ) : (
+        <EmptyList />
+      )}
+    </>
+  )
 }
 
 export default FavoritesPage
