373dd9a61653ef5106ea7a85a0b242b26bcbf7b2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-17 18:11:54 -0400

    refactor: Update buttons component to include CardSubmitButton

--- components/form/buttons.component.tsx
+++ components/form/buttons.component.tsx
@@ -4,7 +4,7 @@ import { useFormStatus } from 'react-dom'
 import { Button } from '../ui/button'
 import { ReloadIcon } from '@radix-ui/react-icons'
 import { SignInButton } from '@clerk/nextjs'
-import { FaRegHeart } from 'react-icons/fa'
+import { FaHeart, FaRegHeart } from 'react-icons/fa'
 
 type SubmitButtonProps = {
   className?: string
@@ -53,6 +53,27 @@ export const CardSignInButton = () => {
         <FaRegHeart />
       </Button>
     </SignInButton>
+  )
+}
+
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus()
+
+  return (
+    <Button
+      type='submit'
+      size='icon'
+      variant='outline'
+      className='p-2 cursor-pointer'
+    >
+      {pending ? (
+        <ReloadIcon className='animate-spin' />
+      ) : isFavorite ? (
+        <FaHeart />
+      ) : (
+        <FaRegHeart />
+      )}
+    </Button>
   )
 }
 
