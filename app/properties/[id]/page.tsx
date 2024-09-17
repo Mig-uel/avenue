f9896789267ec81c6f64faf7ef3699e4ed70c5eb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-17 19:03:48 -0400

    feat: Add Breadcrumbs component, FavoriteButton and property details layout
    Add the Breadcrumbs component and FavoriteButton to the PropertyDetailsPage layout. Display the property name and tagline in the header section. Also, include the dynamic rendering of the favorite button for the property.

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,3 +1,5 @@
+import FavoriteButton from '@/components/card/favorite-button.component'
+import Breadcrumbs from '@/components/properties/breadcrumbs.component'
 import { fetchPropertyDetails } from '@/utils/actions/property/action'
 import { redirect } from 'next/navigation'
 
@@ -10,9 +12,22 @@ const PropertyDetailsPage = async ({
 
   if (!property) redirect('/')
 
-  const { baths, bedrooms, beds, guests } = property
+  const { name, baths, bedrooms, beds, guests, tagline } = property
   const details = { baths, bedrooms, beds, guests }
 
-  return <div>PropertyDetailsPage</div>
+  return (
+    <section>
+      <Breadcrumbs name={name} />
+
+      <header className='flex justify-between items-center mt-4'>
+        <h1 className='text-4xl font-bold capitalize'>{tagline}</h1>
+
+        <div className='flex items-center gap-x-4'>
+          {/* TODO: share button */}
+          <FavoriteButton propertyId={property.id} />
+        </div>
+      </header>
+    </section>
+  )
 }
 export default PropertyDetailsPage
