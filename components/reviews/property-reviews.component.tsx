ac40f403e989398d1bbaaaff4b37520e8ac00639
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-19 12:35:10 -0400

    Add PropertyReviews component for displaying property reviews

---
+++ components/reviews/property-reviews.component.tsx
@@ -0,0 +1,30 @@
+import { fetchPropertyReviews } from '@/utils/actions/reviews/action'
+import Title from '../properties/title.component'
+import ReviewCard from './review-card.components'
+
+const PropertyReviews = async ({ propertyId }: { propertyId: string }) => {
+  const reviews = await fetchPropertyReviews(propertyId)
+
+  if (!reviews) return null
+
+  return (
+    <div className='mt-8'>
+      <Title text='Reviews' />
+
+      <div className='grid md:grid-cols-2 gap-8 mt-4'>
+        {reviews.map((review, index) => {
+          const {
+            comment,
+            rating,
+            profile: { firstName: name, profileImage: image },
+          } = review
+
+          const reviewInfo = { comment, rating, name, image }
+
+          return <ReviewCard {...reviewInfo} />
+        })}
+      </div>
+    </div>
+  )
+}
+export default PropertyReviews
