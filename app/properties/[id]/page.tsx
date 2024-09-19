0f0a190d49acd8fd5529b83d7fd43c9a38eb3cd2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-19 14:56:45 -0400

    Refactor PropertyDetailsPage component to conditionally render BookingCalendar and SubmitReview based on user authentication status

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -13,6 +13,7 @@ import SubmitReview from '@/components/reviews/submit-review.component'
 import { Separator } from '@/components/ui/separator'
 import { Skeleton } from '@/components/ui/skeleton'
 import { fetchPropertyDetails } from '@/utils/actions/property/action'
+import { currentUser } from '@clerk/nextjs/server'
 import dynamic from 'next/dynamic'
 import { redirect } from 'next/navigation'
 
@@ -29,6 +30,7 @@ const PropertyDetailsPage = async ({
 }: {
   params: { id: string }
 }) => {
+  const user = await currentUser()
   const property = await fetchPropertyDetails(id)
 
   if (!property) redirect('/')
@@ -62,7 +64,7 @@ const PropertyDetailsPage = async ({
       <ImageContainer src={image} alt={name} />
 
       <section className='lg:grid lg:grid-cols-12 gap-x-12 mt-12'>
-        <div className='lg:col-span-8'>
+        <div className={`lg:col-span-${user ? '8' : '12'}`}>
           <div className='flex gap-x-4 items-center'>
             <h1 className='text-xl font-bold'>{property.name}</h1>
             <PropertyRating inPage propertyId={property.id} />
@@ -75,12 +77,13 @@ const PropertyDetailsPage = async ({
           <DynamicMap countryCode={property.country} />
         </div>
 
-        <div className='lg:col-span-4 flex flex-col items-center'>
-          <BookingCalendar />
-        </div>
+        {user && (
+          <div className='lg:col-span-4 flex flex-col items-center'>
+            <BookingCalendar />
+          </div>
+        )}
       </section>
-
-      <SubmitReview propertyId={property.id} />
+      {user && <SubmitReview propertyId={property.id} />}
       <PropertyReviews propertyId={property.id} />
     </section>
   )
