aa756dc2b36abb62abb7edb2a7e2de37eacb137a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-20 14:28:28 -0400

    Refactor: Update BookingCalendar component to disable blocked periods and handle unavailable dates

--- components/booking/booking-calendar.component.tsx
+++ components/booking/booking-calendar.component.tsx
@@ -1,12 +1,19 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { defaultSelected, generateBlockedPeriods } from '@/utils/calendar'
+import {
+  defaultSelected,
+  generateBlockedPeriods,
+  generateDateRange,
+  generateDisabledDates,
+} from '@/utils/calendar'
 import type { DateRange } from 'react-day-picker'
 import { Calendar } from '../ui/calendar'
 import { usePropertyState } from '@/utils/store'
+import { useToast } from '@/hooks/use-toast'
 
 const BookingCalendar = () => {
+  const { toast } = useToast()
   const currentDate = new Date()
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected)
 
@@ -17,7 +24,22 @@ const BookingCalendar = () => {
     today: currentDate,
   })
 
+  const unavailableDates = generateDisabledDates(blockedPeriods)
+
   useEffect(() => {
+    const selectedRange = generateDateRange(range)
+
+    selectedRange.some((date) => {
+      if (unavailableDates[date]) {
+        setRange(defaultSelected)
+        toast({
+          description: 'Selected days are unavailable. Please select again.',
+        })
+        return true
+      }
+      return false
+    })
+
     usePropertyState.setState({ range })
   }, [range])
 
