5bbaf86d156057f171addae20826ed660be9769e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-20 12:12:17 -0400

    Refactor: Update BookingContainer to handle empty date and same day range selection

--- components/booking/booking-container.component.tsx
+++ components/booking/booking-container.component.tsx
@@ -4,9 +4,11 @@ import ConfirmBooking from './confirm-booking.component'
 import BookingForm from './booking-form.component'
 
 const BookingContainer = () => {
-  const state = usePropertyState((state) => state)
+  const range = usePropertyState((state) => state.range)
 
-  console.log(state)
+  if (!range || !range.from || !range.to) return null
+
+  if (range.to.getTime() === range.from.getTime()) return null
 
   return (
     <div className='w-full'>
