ecc327f3fba448e5de441066c0cbb1118b31c448
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-21 16:03:30 -0400

    Refactor fetchStats function to fetch and return bookings per month for chart visualization

--- utils/actions/admin/actions.tsx
+++ utils/actions/admin/actions.tsx
@@ -1,3 +1,4 @@
+import { formatDate } from '@/utils/format'
 import db from '../../db'
 import { getAuthUser } from '@/utils/functions.utils'
 import { redirect } from 'next/navigation'
@@ -11,8 +12,6 @@ export const fetchStats = async () => {
 
   if (!admin.privateMetadata.isAdmin) return redirect('/')
 
-  const adminId = admin.id
-
   const usersCount = await db.profile.count()
   const propertiesCount = await db.property.count()
   const bookingsCount = await db.booking.count()
@@ -22,4 +21,38 @@ export const fetchStats = async () => {
     propertiesCount,
     bookingsCount,
   }
+}
+
+export const fetchChartsData = async () => {
+  const admin = await getAuthUser()
+
+  if (!admin.privateMetadata.isAdmin) return redirect('/')
+
+  const date = new Date()
+  date.setMonth(date.getMonth() - 6)
+
+  const sixMonthsAgo = date
+
+  const bookings = await db.booking.findMany({
+    where: {
+      createdAt: {
+        gte: sixMonthsAgo,
+      },
+    },
+    orderBy: {
+      createdAt: 'asc',
+    },
+  })
+
+  let bookingsPerMonth = bookings.reduce((acc, curr) => {
+    const date = formatDate(curr.createdAt, true)
+    const existingEntry = acc.find((entry) => entry.date === date)
+
+    if (existingEntry) existingEntry.count += 1
+    else acc.push({ date, count: 1 })
+
+    return acc
+  }, [] as Array<{ date: string; count: number }>)
+
+  return bookingsPerMonth
 }
