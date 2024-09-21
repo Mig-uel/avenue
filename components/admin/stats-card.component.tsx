5c74c07dc56245d82908a9ea28fd46178ff100a3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-21 15:43:32 -0400

    Add StatsCard component for displaying statistics in the admin page

---
+++ components/admin/stats-card.component.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader } from '../ui/card'
+
+const StatsCard = ({ title, value }: { title: string; value: number }) => {
+  return (
+    <Card className='bg-muted'>
+      <CardHeader className='flex flex-row items-center justify-between'>
+        <h3 className='font-bold text-3xl capitalize'>{title}</h3>
+        <span className='text-primary text-5xl font-extrabold'>{value}</span>
+      </CardHeader>
+    </Card>
+  )
+}
+export default StatsCard
