d31fcf10fd149c3902b146f5d8a528d9b73a8489
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-21 15:32:54 -0400

    Refactor admin page component to add loading containers for stats and charts

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,27 @@
+import ChartsContainer from '@/components/admin/charts-container.component'
+import {
+  ChartsLoadingContainer,
+  StatsLoadingContainer,
+} from '@/components/admin/loading.component'
+import StatsContainer from '@/components/admin/stats-container.component'
 import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
+import { Suspense } from 'react'
 
 const AdminPage = async () => {
   const user = await currentUser()
 
   if (!user?.privateMetadata?.isAdmin) redirect('/')
 
-  return <div>AdminPage</div>
+  return (
+    <>
+      <Suspense fallback={<StatsLoadingContainer />}>
+        <StatsContainer />
+      </Suspense>
+      <Suspense fallback={<ChartsLoadingContainer />}>
+        <ChartsContainer />
+      </Suspense>
+    </>
+  )
 }
 export default AdminPage
