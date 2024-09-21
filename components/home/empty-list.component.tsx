9b507e5bb507d3264e36fc749e7eb2c29a738bd4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-20 21:02:01 -0400

    Refactor: Add href prop to EmptyList component

--- components/home/empty-list.component.tsx
+++ components/home/empty-list.component.tsx
@@ -5,19 +5,21 @@ type EmptyList = {
   heading?: string
   message?: string
   buttonText?: string
+  href?: string
 }
 
 const EmptyList = ({
   heading = 'No items in the list',
   message = 'Keep exploring our properties',
   buttonText = 'Back Home',
+  href = '/',
 }: EmptyList) => {
   return (
     <div className='mt-4'>
       <h2 className='text-xl font-bold'>{heading}</h2>
       <p className='text-lg'>{message}</p>
       <Button asChild className='mt-4 capitalize' size='lg'>
-        <Link href='/'>{buttonText}</Link>
+        <Link href={href}>{buttonText}</Link>
       </Button>
     </div>
   )
