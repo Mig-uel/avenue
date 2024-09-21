d4e86beeef458a0177ab42a332f1c18dac6c485c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-20 20:55:44 -0400

    Refactor: Add deleteRentalAction and fetchRentals functions to rentals/actions.ts

---
+++ utils/actions/rentals/actions.ts
@@ -0,0 +1,81 @@
+import { revalidatePath } from 'next/cache'
+import db from '../../db'
+import { errorMessage, getAuthUser } from '@/utils/functions.utils'
+/**
+ * DELETE RENTALS BY PROPERTY ID AND USER ID
+ * @param prevState
+ * @returns
+ */
+export const deleteRentalAction = async (prevState: { propertyId: string }) => {
+  try {
+    const user = await getAuthUser()
+    const { propertyId } = prevState
+
+    const property = await db.property.delete({
+      where: {
+        id: propertyId,
+        profileId: user.id,
+      },
+      select: {
+        name: true,
+      },
+    })
+
+    revalidatePath('/rentals')
+
+    return {
+      message: `Rental '${property.name} was deleted'`,
+    }
+  } catch (error) {
+    return errorMessage(error)
+  }
+}
+
+/**
+ * FETCH RENTALS
+ * @returns
+ */
+export const fetchRentals = async () => {
+  const user = await getAuthUser()
+
+  const rentals = await db.property.findMany({
+    where: {
+      profileId: user.id,
+    },
+    select: {
+      id: true,
+      name: true,
+      price: true,
+    },
+  })
+
+  const rentalsWithBookings = await Promise.all(
+    rentals.map(async (rental) => {
+      const totalNightsSum = await db.booking.aggregate({
+        where: {
+          propertyId: rental.id,
+        },
+        _sum: {
+          totalNights: true,
+        },
+      })
+
+      const orderTotal = await db.booking.aggregate({
+        where: {
+          propertyId: rental.id,
+        },
+        _sum: {
+          orderTotal: true,
+        },
+      })
+
+      return {
+        ...rental,
+        totalNightsSum: totalNightsSum._sum.totalNights,
+        orderTotal: orderTotal._sum.orderTotal,
+      }
+    })
+  )
+
+  return rentalsWithBookings
+}
